e97731f0e139ddecfd6e74455f6e1cbb3122fa70
kristinlin <klin11@stuy.edu>
2017-10-18 15:32:00 -0400

    got rid of free_list

--- player.c
+++ player.c
@@ -2,20 +2,10 @@
 #include <stdlib.h>
 #include "song_node.h"
 
-struct song_node * free_all(struct song_node * pter)
-{
-  struct song_node * temp1 = pter;
-  while (pter) {
-    temp1 = pter->next;
-    free(temp1);
-    pter = temp1;
-  }
-  return pter;
-}
 
 int main() {
   struct song_node * playlist;
-  playlist =
+  playlist = insert_order(NU
 
     
 
