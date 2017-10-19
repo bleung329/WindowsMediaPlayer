51630a7536c4daa549d16d4e78a3e0a864df5c7a
bleung329 <bleung@stuy.edu>
2017-10-19 15:22:11 -0400

    init library

---
+++ library.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "song_node.h"
+
+/*
+    Add song nodes.
+    Search for a song given song and artist name (return a pointer).
+    Search for an artist (return a pointer).
+    Print out all the entries under a certain letter.
+    Print out all the songs of a certain artist
+    Print out the entire library.
+    Shuffle - print out a series of randomly chosen songs.
+    Delete a song
+    Delete all the nodes.
+*/
